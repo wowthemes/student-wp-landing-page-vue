70962fb7eb41c57079c6888c889b249891a6b400
wowthemes <shehbaz2009@gmail.com>
2022-01-11 00:46:58 +0500

    working on hero area

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="site--hero_area">
       <main-header />
-      <hero class="position-relative" />
+      <hero class="position-relative site--hero_content" />
     </div>
   </div>
 </template>
@@ -36,9 +36,15 @@ export default {
     width: 100%;
     height: 100%;
     background-color: #0f1c49;
-    opacity: 0.8;
+    opacity: 0.9;
     top: 0;
     z-index: 0;
+  }
+  .site--hero_content {
+    padding: 120px 0;
+  }
+  .btn--space_extra {
+    padding: 20px 50px;
   }
 }
 </style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-sm-6 text-center">
+      <div class="col-sm-5 text-center">
         <h2 class="text-white">The premium education</h2>
         <h2 class="text-white">WordPress theme of 2022</h2>
 
-        <a class="btn btn-primary mt-4 rounded-pill">View Live demo</a><br>
-        <a class="btn btn-secondary mt-4 rounded-pill">Buy this theme</a>
+        <a class="btn btn-primary mt-4 rounded-pill btn--space_extra">View Live demo</a><br>
+        <a class="btn btn-secondary mt-4 rounded-pill btn--space_extra">Buy this theme</a>
+      </div>
+      <div class="col-sm-7 position-relative">
+        <img src="../assets/images/hero-img-1.png" alt="" />
+        <img src="../assets/images/hero-img-2.png" class="hero--img_2 animate__animated animate__slideInUp" alt="" />
+        <img src="../assets/images/hero-img-3.png" class="hero--img_3 animate__animated animate__slideInRight" alt="" />
+        <img src="../assets/images/elementor.png" class="hero--elementor" alt="" />
       </div>
     </div>
   </div>
 </template>
+
+<style lang="scss">
+.site--hero_content {
+  overflow: hidden;
+  .hero--img_2 {
+    position: absolute;
+    left: 5%;
+    bottom: -10%;
+  }
+  .hero--img_3 {
+    position: absolute;
+    right: 0;
+    top: 10%;
+  }
+  .hero--elementor {
+    position: absolute;
+    bottom: -7%;
+    right: 25%;
+  }
+}
+</style>
